4fb3626f109893b5f348ad8e057adbf49b53303a
dean_akinwale <dean.akinwale@jet2.com>
2020-09-04 09:57:41 +0100

    wip

--- 02-getting-started/end/vue-heroes/src/components/header-bar-links.vue
+++ 02-getting-started/end/vue-heroes/src/components/header-bar-links.vue
@@ -3,7 +3,7 @@
     <div class="navbar-end">
       <div class="navbar-item">
         <div class="buttons">
-          <a :href="github" target="_blank" rel="noopener noreferrer">
+          <a v-bind:href="github" target="_blank" rel="noopener noreferrer">
             <i class="fab fa-github fa-2x" aria-hidden="true"></i>
           </a>
           <a :href="twitter" target="_blank" rel="noopener noreferrer">
@@ -19,8 +19,8 @@
 export default {
   data() {
     return {
-      github: 'https://github.com/johnpapa/vue-getting-started',
-      twitter: 'https://twitter.com/john_papa',
+      github: 'https://github.com/johnpapa/vue-getting-starte',
+      twitter: 'https://twitter.com/john_pap',
     };
   },
 };
